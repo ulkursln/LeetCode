7b12cc5e985456a7685235ae324316ca1ec7c288
Ülkü Arslan Aydın <ulkursln@users.noreply.github.com>
2022-10-16 21:47:33 +0200

    Create LC_49.java

---
+++ LC_49.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap();
+        
+        for(String str: strs){
+            int[] chars = new int[26];
+            for(char ch: str.toCharArray()){
+                chars[ch-'a']++;
+            }
+            
+            StringBuilder key = new StringBuilder();
+            for(int ch: chars){
+                key.append(ch); key.append("-");
+            }
+            
+            List<String> anagrams = map.getOrDefault(key.toString(), new ArrayList());
+            anagrams.add(str);
+            map.put(key.toString(), anagrams);
+        }
+        
+        List<List<String>> ans = new ArrayList();
+        for(String key: map.keySet()){
+            ans.add(map.get(key));
+        }
+        
+        return ans;
+        
+    }
+}
