264e7a213f9afb10ac8d3593351b80a65d395d07
Ülkü Arslan Aydın <ulkursln@users.noreply.github.com>
2023-04-09 11:24:03 +0200

    Update LC_202.java

--- LC75/Level2/LC_202.java
+++ LC75/Level2/LC_202.java
@@ -1,3 +1,18 @@
 // Implementation/Simulation
 // Happy Number
 
+  public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        int total = 0;
+        while( n != 1){
+            if(set.contains(n)) return false;
+            set.add(n);
+            total = 0;
+            while(n > 0){
+                total += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = total;
+        }
+        return true;
+    }
