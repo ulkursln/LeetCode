c4769782e881e0b8c6f325463a2a4b7de525b3f2
Ülkü Arslan Aydın <ulkursln@users.noreply.github.com>
2023-04-09 12:19:35 +0200

    Update LC_202.java

--- LC75/Level2/LC_202.java
+++ LC75/Level2/LC_202.java
@@ -1,6 +1,7 @@
 // Implementation/Simulation
 // Happy Number
 
+//SC = O(logn?)
   public boolean isHappy(int n) {
         Set<Integer> set = new HashSet<>();
         int total = 0;
@@ -15,4 +16,26 @@
             n = total;
         }
         return true;
+    }
+
+//SC = O(1)
+
+public boolean isHappy(int n) {
+        
+        int slow = n, fast = n;
+        while( slow !=1 && fast != 1){
+            slow = helper(slow);
+            fast = helper(helper(fast));
+            if(fast == slow  && fast !=1) return false;
+        }
+        return true;
+    }
+
+    int helper(int n){
+        int total = 0;
+        while(n > 0){
+                total += (n % 10) * (n % 10);
+                n /= 10;
+        }
+        return total;
     }
