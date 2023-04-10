d6adfa919a3443155b3bcf1649e5c22fe07c56ae
Ülkü Arslan Aydın <ulkursln@users.noreply.github.com>
2023-04-10 10:50:00 +0200

    Update LC_1706.java

--- LC75/Level2/LC_1706.java
+++ LC75/Level2/LC_1706.java
@@ -29,3 +29,31 @@ public int[] findBall(int[][] grid) {
 
         return ans;
     }
+
+//simplfy if condition:
+public int[] findBall(int[][] grid) {
+        int col = grid[0].length;
+        int row = grid.length;
+        int[] ans = new int[col];
+        Arrays.fill(ans, -2);
+
+        int pos= 0;
+
+        for(int i = 0; i < col; i++){
+            pos = i;
+            for(int j = 0; j < row; j++){
+                int val=grid[j][pos];
+                if( pos + val < 0 || pos+val >= col || grid[j][pos+val] != val)    
+                {
+                    ans[i] = -1;
+                    break;
+                }
+           
+                pos = pos+val;
+                
+            }
+            ans[i]= ans[i]==-2?pos: ans[i];
+        }
+
+        return ans;
+    }
