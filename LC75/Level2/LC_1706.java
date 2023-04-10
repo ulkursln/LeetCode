0dc56fca4214895a5cc66fcb79f605e2d06b4c27
Ülkü Arslan Aydın <ulkursln@users.noreply.github.com>
2023-04-10 08:41:35 +0200

    Create LC_1706.java

---
+++ LC75/Level2/LC_1706.java
@@ -0,0 +1,31 @@
+//1706. Where Will the Ball Fall
+//Implementation/Simulation
+
+public int[] findBall(int[][] grid) {
+        int col = grid[0].length;
+        int row = grid.length;
+        int[] ans = new int[col];
+        Arrays.fill(ans, -2);
+
+        int pos= 0;
+
+        for(int i = 0; i < col; i++){
+            pos = i;
+            for(int j = 0; j < row; j++){
+                int val=grid[j][pos];
+                if((pos+1 < col && val ==1 && grid[j][pos+1] == -1) || 
+                   (pos-1 >= 0 && val ==-1 && grid[j][pos-1] == 1) ||
+                   ( pos + val < 0 || pos+val >= col))               
+                {
+                    ans[i] = -1;
+                    break;
+                }
+           
+                pos = pos+val;
+                
+            }
+            ans[i]= ans[i]==-2?pos: ans[i];
+        }
+
+        return ans;
+    }
